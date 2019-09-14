78af8fc4b33144184bb813d2c1a58c465c13fb73
Daniel Santos <dfsantosbu@unal.edu.co>
2019-09-14 19:24:14 +0200

    Add some styles to edit input fields.

--- app/src/main/java/me/danieldev/audio/MainActivity.java
+++ app/src/main/java/me/danieldev/audio/MainActivity.java
@@ -6,10 +6,16 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button playSong;
+    private TextInputEditText txtX;
+    private TextInputEditText txtY;
+    private TextInputEditText txtZ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         playSong = findViewById(R.id.btnPlay);
+        txtX = findViewById(R.id.txtX);
+        txtY = findViewById(R.id.txtY);
+        txtZ = findViewById(R.id.txtZ);
 
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.beep1);
 
