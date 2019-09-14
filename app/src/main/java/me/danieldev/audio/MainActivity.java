a7bc8a41bf86b875d6d4205c498d6ff7b688a0d3
Daniel Santos <dfsantosbu@unal.edu.co>
2019-09-14 18:01:32 +0200

    Add behaviour to the play button.

--- app/src/main/java/me/danieldev/audio/MainActivity.java
+++ app/src/main/java/me/danieldev/audio/MainActivity.java
@@ -2,12 +2,30 @@ package me.danieldev.audio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button playSong;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playSong = findViewById(R.id.btnPlay);
+
+       // final MediaPlayer mp = MediaPlayer.create(this, R.raw.)
+
+        playSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
     }
 }
